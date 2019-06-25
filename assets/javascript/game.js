c128a9deff9a89fd2f569dde5abf202ced6a8fea
Jeffrey Davis <davisj0214@yahoo.com>
2019-06-25 17:38:59 -0500

    markup complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// Create an Array of words
+
+var words = ['contra', 'mega man', 'super dodgeball', 'punch out', 'legend of zelda'];
+
+// Choose words randomnly
+
+var randomNumber = Math.floor(Math.random() * words.length);
+var chosenWord = words[randomNumber];
+
+var letterUnderscore = () => {
+    for(var i = 0; i < chosenWord.length; i++) {
+        
+    }
+}
+
+// var randomWord = Math.floor(Math.random() * words.length);
+// console.log(randomWord);
+
+// Create underscores based on the amount of letters in a word
+
+// Get user's guess
+
+// Determine if guess is correct
+
+// If correct, push to current word being guessed
+
+// If incorrect, push to "letters guessed"
+
+// Display the amount of wins
+
