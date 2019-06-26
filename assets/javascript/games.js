3ebffcd75f8a24c7f52578cd9f5462f40d1139f0
Jeffrey Davis <davisj0214@yahoo.com>
2019-06-26 12:57:40 -0500

    some css added

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,11 +1,50 @@
 // Create the variables for the game
-// Create an onkeyup function to show keys pressed
+var wins = 0;
+var correctGuess = [];
+var wrongGuess = [];
+var guessesRemaining = 10;
+var lettersRemaining = [];
+var underscore = [];
+
+// Grab the element from the html page
+var totalWins = document.getElementById('wins-text');
+var replaceUnderscore = document.getElementById('current-word');
+var incorrectGuess = document.getElementById('already-guessed');
+
 // Create the Array of words to be guessed
+var gameWords = ['contra', 'megaman', 'dodgeball', 'gradius', 'zelda'];
+
 // Generate the words randomly
+var nesGame = Math.floor(Math.random() * gameWords.length);
+var chosenWord = gameWords[nesGame];
+
 // Create the underscores for the random words
-// Check for correct guesses
+var letterUnderscore = () => {
+    for(var i = 0; i < chosenWord.length; i++) {
+        underscore.push('_');
+    }
+    return underscore;
+}
+
+// Create a keypress function
+document.addEventListener('keypress', (event) => {
+    var keyWord = String.fromCharCode(event.keyCode);
+// Correct letter chosen    
+    if(chosenWord.indexOf(keyWord) > -1) {
+        correctGuess.push(keyWord); // STOPPED HERE
+        underscore[chosenWord.indexOf(keyWord)] = keyWord;
+// Incorrect letter chosen
+    }  else {
+        wrongGuess.push(keyWord);
+        incorrectGuess[0].innerHTML = wrongGuess;
+    }
+})
+
 // Display the correct guesses in the word
 // Check for incorrect guesses
+
+// =============== DOM Manipulation ==================
 // Display the incorrect guesses in the "Already Guessed" area
 // Display the amount of wins when user wins the game
 // Show image and play audio when user completes a word
+// Allow the user to press a button to play again
