8003ca8f784901009b91562d5b749c1f6b0abead
Jeffrey Davis <davisj0214@yahoo.com>
2019-06-25 19:29:54 -0500

    JS nearly completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,56 @@
-// Create an Array of words
-
-var words = ['contra', 'mega man', 'super dodgeball', 'punch out', 'legend of zelda'];
+// Create the Array of words
+var words = ['contra', 'megaman', 'dodgeball', 'gradius', 'zelda'];
 
 // Choose words randomnly
-
 var randomNumber = Math.floor(Math.random() * words.length);
 var chosenWord = words[randomNumber];
+// Create the variables 
+var wins = 0;
+var lettersRemaining = 10;
+var correct = [];
+var wrong = [];
+var underscore = [];
 
-var letterUnderscore = () => {
-    for(var i = 0; i < chosenWord.length; i++) {
-        
-    }
-}
+var totalWins = document.getElementById('wins-text');
+var replaceUnderscore = document.getElementById('current-word');
+var incorrectGuess = document.getElementById('already-guessed');
 
-// var randomWord = Math.floor(Math.random() * words.length);
-// console.log(randomWord);
 
 // Create underscores based on the amount of letters in a word
 
-// Get user's guess
+var letterUnderscore = () => {
+    for(var i = 0; i < chosenWord.length; i++) {
+        underscore.push('_');
+    }
+    return underscore;
+}
 
-// Determine if guess is correct
+// Get user's guess and determine whether it's correct or wrong
+
+document.addEventListener('keypress', (event) => {
+    var keyword = String.fromCharCode(event.keyCode);
+
+    // This determines if the user is correct or not and places the letter accordingly
+
+    if(chosenWord.indexOf(keyword) > -1) {
+        correct.push(keyword);
+        underscore[chosenWord.indexOf(keyword)] = keyword;
+        replaceUnderscore[0].innerHTML = underscore.join('');
+        if(underscore.join('') == chosenWord) {
+            alert('You Win');
+        }
+    } else { 
+        wrong.push(keyword);
+        incorrectGuess[0].innerHTML = wrong;
+    }
+});
+
+replaceUnderscore[0].innerHTML = letterUnderscore().join('');
+
+
+
+
+
 
 // If correct, push to current word being guessed
 
---
+++ assets/javascript/games.js
@@ -0,0 +1,11 @@
+// Create the variables for the game
+// Create an onkeyup function to show keys pressed
+// Create the Array of words to be guessed
+// Generate the words randomly
+// Create the underscores for the random words
+// Check for correct guesses
+// Display the correct guesses in the word
+// Check for incorrect guesses
+// Display the incorrect guesses in the "Already Guessed" area
+// Display the amount of wins when user wins the game
+// Show image and play audio when user completes a word
